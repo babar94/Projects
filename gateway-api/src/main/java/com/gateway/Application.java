39d11735bdd637f9b30ddc19989f89fd51460ec7
babar.kamal <babar.kamal@paysyslabs.com>
2024-05-23 16:07:14 +0500

    encrypt database password

--- gateway-api/src/main/java/com/gateway/Application.java
+++ gateway-api/src/main/java/com/gateway/Application.java
@@ -1,12 +1,19 @@
 
 package com.gateway;
 
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 @SpringBootApplication(scanBasePackages = { "com.gateway" })
+@EnableEncryptableProperties
+
 public class Application extends SpringBootServletInitializer {
 
 	@Override
@@ -17,6 +24,17 @@ public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	
+
+	@Bean("jasyptStringEncryptor")
+	public StringEncryptor stringEncryptor() {
+		StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+		encryptor.setPassword("dbsecretkey"); // Replace with your encryption key or passphrase
+		encryptor.setAlgorithm("PBEWithMD5AndDES");
+		encryptor.setIvGenerator(new org.jasypt.iv.NoIvGenerator());
+		return encryptor;
+           }
 
 }
 			
